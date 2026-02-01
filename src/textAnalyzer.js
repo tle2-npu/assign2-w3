8438cdc1f3cb0b8fbc0eba9ca05fd80e07882ecf
tle2-npu <tle2@northpark.edu>
2026-02-01 15:26:02 -0600

    test text analyzer functions and export them

--- src/textAnalyzer.js
+++ src/textAnalyzer.js
@@ -29,3 +29,26 @@ function countLines(text) {
   const lines = text.split('\n');
   return lines.length;
 }
+
+// Test with quotes.txt
+const quotesText = readFile('./data/quotes.txt');
+
+console.log('quotes.txt');
+console.log('Word count:', countWords(quotesText));
+console.log('Longest word:', findLongestWord(quotesText));
+console.log('Line count:', countLines(quotesText));
+
+// Test with sample-text.txt
+const sampleText = readFile('./data/sample-text.txt');
+
+console.log('sample-text.txt');
+console.log('Word count:', countWords(sampleText));
+console.log('Longest word:', findLongestWord(sampleText));
+console.log('Line count:', countLines(sampleText));
+
+// Export functions
+module.exports = {
+    countWords,
+    findLongestWord,
+    countLines
+};
